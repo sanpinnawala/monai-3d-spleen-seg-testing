3c6cc677d9e44db05e85602b960cf773763ef2bd
Sanduni Pinnawala <sanduni.pinnawala@outlook.com>
2023-06-23 00:55:31 +0100

    Ignore bash scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker_tag=aicregistry:5000/${USER}/test-img:v1
-
-docker build --no-cache . -f Dockerfile \
- -t ${docker_tag} \
- --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --network=host
-docker push ${docker_tag}
